ffda66f9c19186abd9f539a7865919144ce3b7cc
Jeffrey Walls <jeff.walls@hpe.com>
2015-12-03 15:50:40 -0700

    Making it compile again

--- Loader.go
+++ Loader.go
@@ -3,11 +3,13 @@ package main
 func Loader() (Model, error) {
 	defer Trace(Enter())
 
-	if model, err := ConfigLoader(); err != nil {
-		LogFatal(err.Error)
-	}
+	var model Model
 
-	Log(model)
+	//	if model, err := ConfigLoader(); err != nil {
+	//		LogFatal(err.Error)
+	//	}
+
+	//	Log(model)
 
 	return model, nil
 }
